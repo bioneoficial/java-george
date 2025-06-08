495cf9cd1d25700a3579aed92159e4ba9ee3f0c6
bioneoficial <joao.bione@icloud.com>
2025-06-08 19:14:31 -0300

    Add Maven Wrapper, Spring Boot project setup, and initial configuration
    - Introduce Maven Wrapper for consistent build environment.
    - Setup Spring Boot application with initial dependencies.
    - Configure H2 database for in-memory storage.
    - Add gitignore and gitattributes for project housekeeping.
    - Include basic testing framework with a placeholder test.

---
+++ src/main/java/com/javageorge/javageorge/JavaGeorgeApplication.java
@@ -0,0 +1,13 @@
+package com.javageorge.javageorge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaGeorgeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaGeorgeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/javageorge/javageorge/JavaGeorgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javageorge.javageorge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaGeorgeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
